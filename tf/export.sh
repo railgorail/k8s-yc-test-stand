c7a88d77ddb644ee179d76a8389c418ed165970f
Rail <railgorail@gmail.com>
2026-02-01 21:37:06 +0300

    docs: tip

--- tf/export.sh
+++ tf/export.sh
@@ -1,3 +1,4 @@
+# adjust to your service account id
 export YC_TOKEN=$(yc iam create-token --impersonate-service-account-id ajeic7ote9k0is6frmoe)
 export YC_CLOUD_ID=$(yc config get cloud-id)
 export YC_FOLDER_ID=$(yc config get folder-id)
